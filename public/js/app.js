dbfc29c3e74836b25fa9ee9a228f2a5d5c5eae36
U-DESKTOP-2DU6IU9\Alex <nothingsfyteam@gmail.com>
2026-02-16 19:39:04 +0100

    Add TURN servers for WebRTC NAT traversal - fixes audio/video/screenshare

--- public/js/app.js
+++ public/js/app.js
@@ -1094,6 +1094,7 @@
     if (audio) audio.remove();
   }
 
+
   // --- WebRTC Mesh Logic ---
   const iceConfig = {
     iceServers: [
@@ -1101,7 +1102,23 @@
       { urls: 'stun:stun1.l.google.com:19302' },
       { urls: 'stun:stun2.l.google.com:19302' },
       { urls: 'stun:stun3.l.google.com:19302' },
-      { urls: 'stun:stun4.l.google.com:19302' }
+      { urls: 'stun:stun4.l.google.com:19302' },
+      // FREE TURN servers for NAT traversal (critical for production)
+      {
+        urls: 'turn:openrelay.metered.ca:80',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:443',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      },
+      {
+        urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      }
     ],
     iceCandidatePoolSize: 10
   };
